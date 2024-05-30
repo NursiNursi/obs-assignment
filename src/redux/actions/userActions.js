9388e57361f7677834266849357f8492a076bd1f
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2024-05-30 12:18:27 +0700

    Update Redux to Add New User

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,8 +1,15 @@
-import { FETCH_USERS } from "../action-types/userActionTypes";
+import { FETCH_USERS, ADD_USER } from "../action-types/userActionTypes";
 
 export const fetchAllUsers = (data) => {
   return {
     type: FETCH_USERS,
+    payload: { data },
+  };
+};
+
+export const addUser = (data) => {
+  return {
+    type: ADD_USER,
     payload: { data },
   };
 };
--- src/redux/reducers/userReducers.js
+++ src/redux/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { FETCH_USERS } from "../action-types/userActionTypes";
+import { FETCH_USERS, ADD_USER } from "../action-types/userActionTypes";
 
 const initialState = {
   userData: [],
@@ -8,6 +8,8 @@ export const UserReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USERS:
       return { ...state, userData: action.payload.data };
+    case ADD_USER:
+      return { ...state, userData: [...state.userData, action.payload.data] };
 
     default:
       return state;
